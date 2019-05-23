aeb30e3428e4a983dd5eebeba11f17f9e2ca0a45
Geksogod <41061522+Geksogod@users.noreply.github.com>
2019-05-23 23:30:44 +0300

    Revert "commit"
    This reverts commit d6001e3d8a29d2c2e13873dffdaad4e715b9484f, reversing
    changes made to 579871347c20a7e231718b3598e2c1b24a922211.

--- Assets/Resources.cs
+++ Assets/Resources.cs
@@ -1,32 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Resources : MonoBehaviour
 {
-<<<<<<< HEAD
     public static int wood;
-=======
->>>>>>> 99d43a3d9705488fe246dd03989c9325a7f27ff2
     public enum Resource
     {
         Wood
     }
 
-<<<<<<< HEAD
 
 
 
     
-=======
-    public void ChangeCounter(Resource resource)
-    {
-        switch (resource)
-        {
-            case Resource.Wood:
-                wood++;
-                woodCounter.text = wood.ToString();
-                break;
-        }
-    }
->>>>>>> 99d43a3d9705488fe246dd03989c9325a7f27ff2
 }
